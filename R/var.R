6ae4a6a39fa437acef84124ca3e4a6b19880e5de
Xavier Robin <xavalias-github@xavier.robin.name>
2012-07-27 11:41:18 +0200

    Fix broken var.auc function

--- R/var.R
+++ R/var.R
@@ -26,12 +26,12 @@ var.default <- function(...) {
 
 var.auc <- function(auc, ...) {
   # Change roc from an auc to a roc object but keep the auc specifications
-  auc <- roc
+  roc <- auc
   attr(auc, "roc") <- NULL
   roc <- attr(roc, "roc")
   roc$auc <- auc
   # Pass to var.roc
-  var.roc(auc, ...)
+  var.roc(roc, ...)
 }
 
 var.smooth.roc <- function(smooth.roc, ...) {
