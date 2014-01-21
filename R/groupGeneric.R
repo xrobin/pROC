8d0e7320e8bcada099032a5d858bfd00e3f2ee01
Xavier Robin <xavalias-github@xavier.robin.name>
2014-01-21 22:26:47 +0100

    Handling Math and operations correctly on 'auc' objects

---
+++ R/groupGeneric.R
@@ -0,0 +1,29 @@
+# pROC: Tools Receiver operating characteristic (ROC curves) with
+# (partial) area under the curve, confidence intervals and comparison. 
+# Copyright (C) 2014 Xavier Robin
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Ops.auc <- function(e1, e2) {
+  if (is(e1, "auc"))
+    attributes(e1) <- NULL
+  if (is(e2, "auc"))
+   attributes(e2) <- NULL
+  NextMethod()
+}
+
+Math.auc <- function(x, ...) {
+  attributes(x) <- NULL
+  NextMethod()
+}
