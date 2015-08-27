8ca41d19551832048a3a8074cdab3ac5f0a446d0
Xavier Robin <xavalias-github@xavier.robin.name>
2015-08-27 19:17:24 +0200

    ggplot multiple ROC curves from list

--- R/ggplot.R
+++ R/ggplot.R
@@ -1,9 +1,13 @@
-ggplot.roc <- function(data, ...) {
-	# Get the roc data with coords
-	df <- as.data.frame(t(coords(data, "all")), row.names = NA)
 
+#' Returns the coords as a data.frame in the right ordering for ggplot2 
+get.coords.for.ggplot <- function(roc) {
+	df <- as.data.frame(t(coords(roc, "all")), row.names = NA)
+	return(df[rev(seq(nrow(df))),])
+}
+
+get.aes.for.ggplot <- function(roc) {
 	# Prepare the aesthetics
-	if(data$percent) {
+	if(roc$percent) {
 		aes <- ggplot2::aes_string(x = "specificity", y = "sensitivity")
 		xlims <- scale_x_reverse(lim=c(100, 0))
 	}
@@ -11,10 +15,55 @@ ggplot.roc <- function(data, ...) {
 		aes <- ggplot2::aes_string(x = "specificity", y = "sensitivity")
 		xlims <- scale_x_reverse(lim=c(1, 0))
 	}
+	return(list(aes=aes, xlims=xlims))
+}
+
+
+ggplot.roc <- function(data, ...) {
+	# Get the roc data with coords
+	df <- get.coords.for.ggplot(data)
+
+	# Prepare the aesthetics
+	aes <- get.aes.for.ggplot(data)
 
 	# Do the plotting
-	ggplot(df[rev(seq(nrow(df))),]) + ggplot2::geom_line(aes, ...) + xlims
+	ggplot(df) + ggplot2::geom_line(aes$aes, ...) + aes$xlims()
 		
 	# Or with ggvis:
 	# ggvis(df[rev(seq(nrow(df))),], ~1-specificity, ~sensitivity) %>% layer_lines()
+}
+
+ggplot.list <- function(data, ...) {
+	# Make sure data is a list and every element is a roc object
+	if (! all(sapply(data, is, "roc"))) {
+		stop("All elements in 'data' must be 'roc' objects.")
+	}
+	
+	# Make sure percent is consistent
+	percents <- sapply(data, `[[`, "percent")
+	if (!(all(percents) || all(!percents))) {
+		stop("ROC curves use percent inconsistently and cannot be plotted together")
+	}
+	
+	# Make sure the data is a named list
+	if (is.null(names(data))) {
+		names(data) <- seq(data)
+	}
+	# Get the coords
+	coord.dfs <- sapply(data, get.coords.for.ggplot, simplify = FALSE)
+	
+	# Add a "name" colummn
+	for (i in seq_along(coord.dfs)) {
+		coord.dfs[[i]]$name <- names(coord.dfs)[i]
+	}
+	
+	# Make a big data.frame
+	coord.dfs <- dplyr::rbind_all(coord.dfs)
+	
+	# Prepare the aesthetics
+	aes <- get.aes.for.ggplot(data[[1]])
+
+	# Do the plotting
+	ggplot(coord.dfs, aes$aes) + ggplot2::geom_line(aes(color=name)) + aes$xlims
+
 }
