5118aea79654658b9b7b60185566907b21e586b8
Xavier Robin <xavalias-github@xavier.robin.name>
2017-01-29 21:10:45 +0100

    Add tests for deLong Placements

---
+++ tests/testthat/test-deLongPlacementsCpp.R
@@ -0,0 +1,46 @@
+library(pROC)
+data(aSAH)
+r_ndka <- roc(aSAH$outcome, aSAH$ndka)
+r_wfns <- roc(aSAH$outcome, aSAH$wfns)
+r_s100b <- roc(aSAH$outcome, aSAH$s100b)
+
+context("DeLong Placements C++ code works")
+
+for (marker in c("ndka", "wfns", "s100b")) {
+	desc <- sprintf("delongPlacementsCpp runs with %s", marker)
+	r <- roc(aSAH$outcome, aSAH[[marker]])
+	test_that(desc, {
+		placements <- pROC:::delongPlacementsCpp(r)
+		expect_identical(names(placements), c("theta", "X", "Y"))
+		expect_equal(length(placements$theta), 1)
+		expect_equal(length(placements$X), sum(aSAH$outcome == "Poor"))
+		expect_equal(length(placements$Y), sum(aSAH$outcome == "Good"))
+	})
+}
+
+for (marker in c("ndka", "wfns", "s100b")) {
+	desc <- sprintf("delongPlacementsCpp runs with reversed levels and %s", marker)
+	r <- roc(aSAH$outcome, aSAH[[marker]], levels = c("Poor", "Good"))
+	test_that(desc, {
+		placements <- pROC:::delongPlacementsCpp(r)
+		expect_identical(names(placements), c("theta", "X", "Y"))
+		expect_equal(length(placements$theta), 1)
+		expect_equal(length(placements$X), sum(aSAH$outcome == "Good"))
+		expect_equal(length(placements$Y), sum(aSAH$outcome == "Poor"))
+	})
+}
+
+for (marker in c("ndka", "wfns", "s100b")) {
+	desc <- sprintf("delongPlacementsCpp runs with reversed direction and %s", marker)
+	r <- roc(aSAH$outcome, aSAH[[marker]], direction = ">")
+	test_that(desc, {
+		placements <- pROC:::delongPlacementsCpp(r)
+		expect_identical(names(placements), c("theta", "X", "Y"))
+		expect_equal(length(placements$theta), 1)
+		expect_equal(length(placements$X), sum(aSAH$outcome == "Poor"))
+		expect_equal(length(placements$Y), sum(aSAH$outcome == "Good"))
+	})
+}
+
+
+
