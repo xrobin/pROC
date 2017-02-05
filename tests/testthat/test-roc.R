43307f71e82f84ef8c9f2c82d125397a37acf713
Xavier Robin <xavalias-github@xavier.robin.name>
2017-02-05 15:19:51 +0100

    Automated tests of the 'roc' function

---
+++ tests/testthat/test-roc.R
@@ -0,0 +1,50 @@
+library(pROC)
+data(aSAH)
+
+# Get static, correct output of the function
+source(system.file("extdata", "test-roc-expected.R", package="pROC"))
+
+level.values <- list(
+	forward = c("Good", "Poor"),
+	reversed = c("Poor", "Good")
+)
+
+for (marker in c("ndka", "wfns", "s100b")) {
+	for (levels.direction in names(level.values)) {
+		for (percent in c(FALSE, TRUE)) {
+			for (direction in c("auto", "<", ">")) {
+				context(sprintf("'roc' function works with percent = %s, marker = %s, levels.direction = %s, direction = %s", percent, marker, levels.direction, direction))
+				r <- roc(aSAH$outcome, aSAH[[marker]], levels = level.values[[levels.direction]], direction = direction, percent = percent, quiet = TRUE)
+
+				expected.direction <- ifelse(direction == "auto", ifelse(levels.direction == "forward", "<", ">"), direction)
+				
+				expect_is(r, "roc")
+				expect_identical(r$percent, percent)
+				expect_identical(r$direction, expected.direction)
+				expect_identical(r$levels, level.values[[levels.direction]])
+				
+				expect_equal(r$thresholds, expected.roc[[marker]][[levels.direction]][[expected.direction]][["thresholds"]])
+				expect_equal(r$sensitivities, expected.roc[[marker]][[levels.direction]][[expected.direction]][["sensitivities"]] * ifelse(percent, 100, 1))
+				expect_equal(r$specificities, expected.roc[[marker]][[levels.direction]][[expected.direction]][["specificities"]] * ifelse(percent, 100, 1))
+			}
+		}
+	}
+}
+
+# The code below can be used to refresh the "expected.roc" data, just in case...
+# expected.roc <- list()
+# for (marker in c("ndka", "wfns", "s100b")) {
+# 	expected.roc[[marker]] <- list()
+# 	for (levels.direction in names(level.values)) {
+# 		expected.roc[[marker]][[levels.direction]] <- list()
+# 		for (direction in c("<", ">")) {
+# 			r <- roc(aSAH$outcome, aSAH[[marker]], levels = level.values[[levels.direction]], direction = direction, percent = FALSE, quiet = TRUE)
+# 			if (!isTRUE(percent) && direction != "auto") {
+# 				expected.roc[[marker]][[levels.direction]][[direction]] <- r[c("sensitivities", "specificities", "thresholds")]
+# 				expected.roc[[marker]][[levels.direction]][[direction]][["auc"]] <- as.numeric(r$auc)
+# 			}
+# 		}
+# 	}
+# }
+# save("expected.roc", system.file("extdata", "test-roc-expected.R", package="pROC"), file = "dump_roc_expected.R")
+
