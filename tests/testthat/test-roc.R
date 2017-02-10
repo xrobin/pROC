ddbb2cafad94ed53837f124aae9bf409b3bd1324
Xavier Robin <xavalias-github@xavier.robin.name>
2017-02-10 22:53:09 +0100

    Removed algorithm in this branch

--- tests/testthat/test-roc.R
+++ tests/testthat/test-roc.R
@@ -9,42 +9,32 @@ level.values <- list(
 	reversed = c("Poor", "Good")
 )
 
-expected.algorithm <- list(
-	pROC:::roc.utils.perfs.all.safe,
-	pROC:::roc.utils.perfs.all.fast,
-	pROC:::rocUtilsPerfsAllC,
-	pROC:::roc.utils.perfs.all.test
-)
-
 for (marker in c("ndka", "wfns", "s100b")) {
 	for (levels.direction in names(level.values)) {
 		for (percent in c(FALSE, TRUE)) {
 			for (direction in c("auto", "<", ">")) {
-				for (algorithm in 1:4) {
-					context(sprintf("'roc' function works with percent = %s, marker = %s, levels.direction = %s, direction = %s and algorithm = %s", percent, marker, levels.direction, direction, algorithm))
-					r <- roc(aSAH$outcome, aSAH[[marker]], levels = level.values[[levels.direction]], direction = direction, percent = percent, algorithm = algorithm, quiet = TRUE)
+					context(sprintf("'roc' function works with percent = %s, marker = %s, levels.direction = %s and direction = %s", percent, marker, levels.direction, direction))
+					r <- roc(aSAH$outcome, aSAH[[marker]], levels = level.values[[levels.direction]], direction = direction, percent = percent, quiet = TRUE)
 					
 					test_that("roc.formula produces the same results as roc.default", {
-						rf <- roc(as.formula(sprintf("outcome ~ %s", marker)), data = aSAH, levels = level.values[[levels.direction]], direction = direction, percent = percent, algorithm = algorithm, quiet = TRUE)
+						rf <- roc(as.formula(sprintf("outcome ~ %s", marker)), data = aSAH, levels = level.values[[levels.direction]], direction = direction, percent = percent, quiet = TRUE)
 						expect_is(rf, "roc")
 						expect_equal(as.numeric(rf$auc), as.numeric(r$auc))
 						for (item in c("percent", "sensitivities", "specificities", "thresholds", "direction", "cases", "controls", "fun.sesp")) {
-							expect_identical(rf[[item]], r[[item]], label = sprintf("roc(outcome ~ %s, %s, %s, %s, %s)[[\"%s\"]]", marker, levels.direction, percent, direction, algorithm, item))
+							expect_identical(rf[[item]], r[[item]], label = sprintf("roc(outcome ~ %s, %s, %s, %s)[[\"%s\"]]", marker, levels.direction, percent, direction, item))
 						}
 						for (item in c("original.predictor", "original.response", "predictor", "response", "levels")) {
-							expect_identical(unname(rf[[item]]), unname(r[[item]]), label = sprintf("roc(outcome ~ %s, %s, %s, %s, %s)[[\"%s\"]]", marker, levels.direction, percent, direction, algorithm, item))
+							expect_identical(unname(rf[[item]]), unname(r[[item]]), label = sprintf("roc(outcome ~ %s, %s, %s, %s)[[\"%s\"]]", marker, levels.direction, percent, direction, item))
 						}
-						expect_identical(rf$fun.sesp, expected.algorithm[[algorithm]])
 					})
 					
 					test_that("roc.default works with control/cases as well", {
-						rcs <- roc(controls = r$controls, cases = r$cases, levels = level.values[[levels.direction]], direction = direction, percent = percent, algorithm = algorithm, quiet = TRUE)
+						rcs <- roc(controls = r$controls, cases = r$cases, levels = level.values[[levels.direction]], direction = direction, percent = percent, quiet = TRUE)
 						expect_is(rcs, "roc")
 						expect_equal(as.numeric(rcs$auc), as.numeric(r$auc))
 						for (item in c("percent", "sensitivities", "specificities", "thresholds", "direction", "cases", "controls", "fun.sesp")) {
 							expect_identical(rcs[[item]], r[[item]])
 						}
-						expect_identical(rcs$fun.sesp, expected.algorithm[[algorithm]])
 					})
 					
 					test_that("roc.default produces the expected results", {
@@ -52,7 +42,6 @@ for (marker in c("ndka", "wfns", "s100b")) {
 					
 						expect_is(r, "roc")
 						expect_identical(r$percent, percent)
-						expect_identical(r$fun.sesp, expected.algorithm[[algorithm]])
 						expect_identical(r$direction, expected.direction)
 						expect_identical(r$levels, level.values[[levels.direction]])
 						
@@ -60,7 +49,6 @@ for (marker in c("ndka", "wfns", "s100b")) {
 						expect_equal(r$sensitivities, expected.roc[[marker]][[levels.direction]][[expected.direction]][["sensitivities"]] * ifelse(percent, 100, 1))
 						expect_equal(r$specificities, expected.roc[[marker]][[levels.direction]][[expected.direction]][["specificities"]] * ifelse(percent, 100, 1))
 					})
-				}
 			}
 		}
 	}
@@ -121,17 +109,6 @@ test_that("roc can't take both response/predictor and case/control", {
 	expect_error(roc(aSAH$outcome, aSAH$ndka, controls = aSAH$ndka[aSAH$outcome == "Good"], cases = aSAH$ndka[aSAH$outcome == "Poor"]))
 })
 
-
-test_that("microbenchmark works", {
-	skip_if_not_installed("microbenchmark")
-	# Algorithm 3 (C) should be selected with small low number of thresholds like aSAH$wfns
-	expect_output(r <- roc(aSAH$outcome, aSAH$wfns, algorithm = 0), "Selecting algorithm 3")
-	
-	# Algorithm 2 (R cumsum) should be selected with large datasets with many thresholds
-	# This is going to be slow, so skip unless we're running slow tests
-	skip_if_not(run_slow_tests, message = "Slow test skipped")
-	expect_output(r <- roc(round(runif(10000)), rnorm(10000), algorithm = 0), "Selecting algorithm 2")
-})
 
 # The code below can be used to refresh the "expected.roc" data, just in case...
 # expected.roc <- list()
