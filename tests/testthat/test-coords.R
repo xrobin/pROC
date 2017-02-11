c2ef2d512c28d8de4757f42c54d13089275e9cc9
Xavier Robin <xavalias-github@xavier.robin.name>
2017-02-11 15:10:04 +0100

    Fix test

--- tests/testthat/test-coords.R
+++ tests/testthat/test-coords.R
@@ -11,8 +11,9 @@ test_that("coords with thresholds works", {
 })
 
 test_that("coords with percent works", {
-	obtained <- coords(r.s100b.percent, "all", ret = c("threshold", "specificity", "sensitivity", "accuracy", "tn", "tp",  "fn", "fp", "npv", "ppv", "1-specificity", "1-sensitivity", "1-accuracy", "1-npv", "1-ppv"))
+	obtained.percent <- coords(r.s100b.percent, "all", ret = c("threshold", "specificity", "sensitivity", "accuracy", "tn", "tp",  "fn", "fp", "npv", "ppv", "1-specificity", "1-sensitivity", "1-accuracy", "1-npv", "1-ppv"))
 	# Adjust for percent
-	obtained[c("specificity", "sensitivity", "npv", "ppv", "1-specificity", "1-sensitivity", "1-npv", "1-ppv"),] <- 	obtained[c("specificity", "sensitivity", "npv", "ppv", "1-specificity", "1-sensitivity", "1-npv", "1-ppv"),] / 100
-	expect_equal(obtained, expected.coords)
+	obtained.percent[c("specificity", "sensitivity", "accuracy", "npv", "ppv", "1-specificity", "1-sensitivity", "1-accuracy", "1-npv", "1-ppv"),] <- obtained.percent[c("specificity", "sensitivity", "accuracy", "npv", "ppv", "1-specificity", "1-sensitivity", "1-accuracy", "1-npv", "1-ppv"),] / 100
+	expect_equal(obtained.percent, expected.coords)
 })
+
