252d6051fdae241b42b64035aa95e27f0ee77a99
Xavier Robin <xavalias-github@xavier.robin.name>
2017-06-21 11:08:16 +0200

    More plot tests

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -6,7 +6,20 @@ context("plot")
 # > manage_cases()
 
 test_that("plot draws correctly", {
-	r <- roc(aSAH$outcome, aSAH$s100b)
-	f <- function() plot(r)
-	vdiffr::expect_doppelganger("basic", f)
+	test_basic_plot <- function() plot(r)
+	# S100b
+	r <- r.s100b
+	vdiffr::expect_doppelganger("basic-s100b", test_basic_plot)
+	
+	r <- r.ndka
+	vdiffr::expect_doppelganger("basic-ndka", test_basic_plot)
+	
+	r <- r.wfns
+	vdiffr::expect_doppelganger("basic-wfns", test_basic_plot)
+})
+
+test_that("legacy.axis works correctly", {
+	r <- r.s100b
+	test_legacy.axis_plot <- function() plot(r, legacy.axes=TRUE)
+	vdiffr::expect_doppelganger("legacy.axes", test_legacy.axis_plot)
 })
