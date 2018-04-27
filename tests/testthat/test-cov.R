06048aebd0c557219f2e28e78d7edc2d51a5ca54
Xavier Robin <xavalias-github@xavier.robin.name>
2018-04-27 21:22:54 +0200

    Typo

--- tests/testthat/test-cov.R
+++ tests/testthat/test-cov.R
@@ -1,35 +1,35 @@
 library(pROC)
 data(aSAH)
 
-test_that("var with delong works", {
+test_that("cov with delong works", {
 	expect_equal(cov(r.wfns, r.ndka), -0.000532967856762438)
 	expect_equal(cov(r.ndka, r.s100b), -0.000756164938056579)
 	expect_equal(cov(r.s100b, r.wfns), 0.00119615567376754)
 })
 
 
-test_that("var works with auc and mixed roc/auc", {
+test_that("cov works with auc and mixed roc/auc", {
 	expect_equal(cov(auc(r.wfns), auc(r.ndka)), -0.000532967856762438)
 	expect_equal(cov(auc(r.ndka), r.s100b), -0.000756164938056579)
 	expect_equal(cov(r.s100b, auc(r.wfns)), 0.00119615567376754)
 })
 
 
-test_that("var with delong and percent works", {
+test_that("cov with delong and percent works", {
 	expect_equal(cov(r.wfns.percent, r.ndka.percent), -5.32967856762438)
 	expect_equal(cov(r.ndka.percent, r.s100b.percent), -7.56164938056579)
 	expect_equal(cov(r.s100b.percent, r.wfns.percent), 11.9615567376754)
 })
 
 
-test_that("var with delong, percent and mixed roc/auc works", {
+test_that("cov with delong, percent and mixed roc/auc works", {
 	expect_equal(cov(auc(r.wfns.percent), r.ndka.percent), -5.32967856762438)
 	expect_equal(cov(r.ndka.percent, auc(r.s100b.percent)), -7.56164938056579)
 	expect_equal(cov(auc(r.s100b.percent), auc(r.wfns.percent)), 11.9615567376754)
 })
 
 
-test_that("var with different auc specifications warns", {
+test_that("cov with different auc specifications warns", {
 	expect_warning(cov(r.wfns, r.ndka.percent))
 	expect_warning(cov(r.wfns.percent, r.ndka))
 	# Also mixing auc/roc
