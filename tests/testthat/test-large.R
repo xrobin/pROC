1f57c9b5a9bf29f7c8f39f4f4ee5ea399e2a8654
Xavier Robin <xavalias-github@xavier.robin.name>
2018-05-06 10:53:52 +0200

    Also ensure basic functionality with CI (delong) and partial AUC

--- tests/testthat/test-large.R
+++ tests/testthat/test-large.R
@@ -6,14 +6,18 @@ test_that("roc can deal with 1E5 data points and many thresholds", {
   response <- rbinom(1E5, 1, .5)
   predictor <- rnorm(1E5)
   # ~ 0.6s
-  roc(response, predictor)
+  r <- roc(response, predictor)
+  ci(r)
+  auc(r, partial.auc = c(0.9, 1))
 })
 
 test_that("roc can deal with 1E6 data points and few thresholds", {
   response <- rbinom(1E6, 1, .5)
   predictor <- rpois(1E6, 1)
   # ~ 0.3s
-  roc(response, predictor)
+  r <- roc(response, predictor)
+  ci(r)
+  auc(r, partial.auc = c(0.9, 1))
 })
 
 test_that("roc can deal with 1E7 data points and few thresholds", {
@@ -21,5 +25,7 @@ test_that("roc can deal with 1E7 data points and few thresholds", {
   response <- rbinom(1E7, 1, .5)
   predictor <- rpois(1E7, 1)
   # ~ 3s
-  roc(response, predictor)
+  r <- roc(response, predictor)
+  ci(r)
+  auc(r, partial.auc = c(0.9, 1))
 })
