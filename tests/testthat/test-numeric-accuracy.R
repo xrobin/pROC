a3a8509e6ff822887d1ac6165b166e4566d88ccf
Xavier Robin <xavalias-github@xavier.robin.name>
2018-06-13 21:43:39 +0200

    Test a very hard predictor

--- tests/testthat/test-numeric-accuracy.R
+++ tests/testthat/test-numeric-accuracy.R
@@ -42,3 +42,25 @@ test_that("delong theta is consistent with auc and direction = >", {
 	expect_equal(pROC:::delongPlacements(r2)$theta, 1-as.numeric(auc(r2)))
 	expect_equal(pROC:::delongPlacements(r3)$theta, 1-as.numeric(auc(r3)))
 })
+
+# Test some crazy values
+# Multiple sequencial near-tie that will break the thresholding algorithm at the limits close to +-Inf or 0
+# Compare that with an "easy" curve with values with well defined intermediate averages
+test_that("Hard predictor has same results as easy one", {
+	numacc.predictor.hard <- c(-0x1.fffffffffffffp+1023, -0x1.ffffffffffffep+1023, -0x1.ffffffffffffdp+1023,
+							   -0x0.0000000000003p-1022, -0x0.0000000000002p-1022, -0x0.0000000000001p-1022, -0x0p+0,
+							   0x0p+0, 0x0.0000000000001p-1022, 0x0.0000000000002p-1022, 0x0.0000000000003p-1022,
+							   0x1.ffffffffffffdp+1023, 0x1.ffffffffffffep+1023, 0x1.fffffffffffffp+1023)
+	numacc.predictor.easy <- c(-10, -9, -8,
+							   -0.1, -0.01, -0.001, 0,
+							   0, 0.001, 0.01, 0.1,
+							   8, 9, 10)
+	response <- rbinom(length(numacc.predictor.easy), 1, 0.5)
+	sample.vector <- sample(length(numacc.predictor.easy))
+	roc.hard <- roc(response, numacc.predictor.hard)
+	roc.easy <- roc(response, numacc.predictor.easy)
+	expect_equal(roc.hard$sensitivities, roc.easy$sensitivities)
+	expect_equal(roc.hard$specificities, roc.easy$specificities)
+	expect_equal(roc.hard$direction, roc.easy$direction)
+})
+
