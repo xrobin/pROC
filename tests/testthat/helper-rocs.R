4e875823227bbee21e05f45e1f33c06cb7574dec
Xavier Robin <xavalias-github@xavier.robin.name>
2019-03-04 22:21:33 +0100

    Move ROC curve generation to helper script

---
+++ tests/testthat/helper-rocs.R
@@ -0,0 +1,15 @@
+data(aSAH)
+
+r.wfns <- roc(aSAH$outcome, aSAH$wfns, quiet = TRUE)
+r.ndka <- roc(aSAH$outcome, aSAH$ndka, quiet = TRUE)
+r.s100b <- roc(aSAH$outcome, aSAH$s100b, quiet = TRUE)
+r.wfns.percent <- roc(aSAH$outcome, aSAH$wfns, percent = TRUE, quiet = TRUE)
+r.ndka.percent <- roc(aSAH$outcome, aSAH$ndka, percent = TRUE, quiet = TRUE)
+r.s100b.percent <- roc(aSAH$outcome, aSAH$s100b, percent = TRUE, quiet = TRUE)
+
+r.wfns.partial1 <- roc(aSAH$outcome, aSAH$wfns, quiet = TRUE, partial.auc = c(0.9, 0.99))
+r.ndka.partial1 <- roc(aSAH$outcome, aSAH$ndka, quiet = TRUE, partial.auc = c(0.9, 0.99))
+r.s100b.partial1 <- roc(aSAH$outcome, aSAH$s100b, quiet = TRUE, partial.auc = c(0.9, 0.99))
+r.wfns.percent.partial1 <- roc(aSAH$outcome, aSAH$wfns, percent = TRUE, quiet = TRUE, partial.auc = c(90, 99))
+r.ndka.percent.partial1 <- roc(aSAH$outcome, aSAH$ndka, percent = TRUE, quiet = TRUE, partial.auc = c(90, 99))
+r.s100b.percent.partial1 <- roc(aSAH$outcome, aSAH$s100b, percent = TRUE, quiet = TRUE, partial.auc = c(90, 99))
