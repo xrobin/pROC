356563874461608f643cd97734e068e820a4bd2b
Xavier Robin <xavalias-github@xavier.robin.name>
2019-03-12 15:29:11 +0100

    Hide progress bar from test

--- tests/testthat/test-ci.coords.R
+++ tests/testthat/test-ci.coords.R
@@ -3,6 +3,8 @@ data(aSAH)
 
 context("ci.coords")
 
+# Silence progress bars
+options(pROCProgress = list(name = "none"))
 
 test_that("ci.coords accepts threshold output with x=best", {
 	expect_error(ci.coords(r.wfns, x="best", input="specificity", ret=c("threshold", "specificity", "sensitivity"), boot.n = 1), NA)
