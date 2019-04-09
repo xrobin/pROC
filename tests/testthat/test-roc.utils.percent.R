0e6467b39967ba89b18cab8d2f3964676dd4f248
Xavier Robin <xavalias-github@xavier.robin.name>
2019-04-09 08:15:43 +0200

    Move function to helper

--- tests/testthat/test-roc.utils.percent.R
+++ tests/testthat/test-roc.utils.percent.R
@@ -3,25 +3,6 @@ data(aSAH)
 
 context("roc.utils.percent")
 
-remove.calls.recursive <- function(x) {
-	if (is.null(x)) return(NULL)
-	attr(x, "roc") <- remove.calls.recursive(attr(x, "roc"))
-	attr(x, "auc") <- remove.calls.recursive(attr(x, "auc"))
-	attr(x, "ci") <- remove.calls.recursive(attr(x, "ci"))
-	if (!is.list(x)) return(x)
-	x$roc <- remove.calls.recursive(x$roc)
-	x$auc <- remove.calls.recursive(x$auc)
-	x$ci <- remove.calls.recursive(x$ci)
-	x$call <- NULL
-	return(x)
-}
-
-expect_equal_ignore_call <- function(x, y, ...) {
-	x <- remove.calls.recursive(x)
-	y <- remove.calls.recursive(y)
-	expect_equal(x, y, ...)
-}
-
 test_that("roc.utils.topercent works on full AUC", {
 	expect_equal_ignore_call(pROC:::roc.utils.topercent.roc(r.wfns), r.wfns.percent)
 })
