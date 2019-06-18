bab6397e4b14bc411b49f5a210532e0268e5fc54
Xavier Robin <xavalias-github@xavier.robin.name>
2019-06-18 12:05:13 +0100

    Test issue #57

--- tests/testthat/test-ci.coords.R
+++ tests/testthat/test-ci.coords.R
@@ -13,3 +13,8 @@ test_that("ci.coords accepts threshold output with x=best", {
 test_that("ci.coords rejects threshold output except with x=best", {
 	expect_error(ci.coords(r.wfns, x=0.9, input="specificity", ret=c("threshold", "specificity", "sensitivity"), boot.n = 1))
 })
+
+test_that("ci.coords accepts threshold output with x=best or if input was threshold", {
+	expect_s3_class(ci.coords(r.wfns, x=2, input="threshold", ret=c("threshold", "specificity", "sensitivity"), boot.n = 1), "ci.coords")
+	expect_s3_class(ci.coords(r.wfns, x="best", ret=c("threshold", "specificity", "sensitivity"), boot.n = 1), "ci.coords")
+})
