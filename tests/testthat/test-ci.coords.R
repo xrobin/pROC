08fdc6c9fd4212d7351676f1d5c86f2e69024402
Xavier Robin <xavalias-github@xavier.robin.name>
2020-01-10 16:28:45 +0100

    Test non stratified ci.coords

--- tests/testthat/test-ci.coords.R
+++ tests/testthat/test-ci.coords.R
@@ -19,49 +19,53 @@ test_that("ci.coords accepts threshold output with x=best or if input was thresh
 	expect_s3_class(ci.coords(r.wfns, x="best", ret=c("threshold", "specificity", "sensitivity"), boot.n = 1), "ci.coords")
 })
 
-for (test.roc in list(r.s100b, smooth(r.s100b))) {
-
-	test_that("ci.coords accepts one x and one ret", {
-		obtained <- ci.coords(test.roc, x=0.8, input = "sensitivity", ret="sp", 
-							  boot.n=3, conf.level = .91)
-		expect_equal(attr(obtained, "ret"), "specificity")
-		expect_equal(names(obtained), attr(obtained, "ret"))
-		for (ci.mat in obtained) {
-			expect_equal(dim(ci.mat), c(1, 3))
-			expect_equal(colnames(ci.mat), c("4.5%", "50%", "95.5%"))
-		}
-	})
-	
-	test_that("ci.coords accepts one x and multiple ret", {
-		obtained <- ci.coords(test.roc, x=0.8, input = "sensitivity", ret=c("sp", "ppv", "tp", "1-sensitivity"), 
-							  boot.n=3, conf.level = .91)
-		expect_equal(attr(obtained, "ret"), c("specificity", "ppv", "tp", "1-sensitivity"))
-		expect_equal(names(obtained), attr(obtained, "ret"))
-		for (ci.mat in obtained) {
-			expect_equal(dim(ci.mat), c(1, 3))
-			expect_equal(colnames(ci.mat), c("4.5%", "50%", "95.5%"))
-		}
-	})
-	
-	test_that("ci.coords accepts multiple x and one ret", {
-		obtained <- ci.coords(test.roc, x=c(0.8, 0.9), input = "sensitivity", ret="sp", 
-							  boot.n=3, conf.level = .91)
-		expect_equal(attr(obtained, "ret"), "specificity")
-		expect_equal(names(obtained), attr(obtained, "ret"))
-		for (ci.mat in obtained) {
-			expect_equal(dim(ci.mat), c(2, 3))
-			expect_equal(colnames(ci.mat), c("4.5%", "50%", "95.5%"))
-		}
-	})
-	
-	test_that("ci.coords accepts multiple x and ret", {
-		obtained <- ci.coords(test.roc, x=c(0.9, 0.8), input = "sensitivity", ret=c("sp", "ppv", "tp", "1-se"), 
-							  boot.n=3, conf.level = .91)
-		expect_equal(attr(obtained, "ret"), c("specificity", "ppv", "tp", "1-sensitivity"))
-		expect_equal(names(obtained), attr(obtained, "ret"))
-		for (ci.mat in obtained) {
-			expect_equal(dim(ci.mat), c(2, 3))
-			expect_equal(colnames(ci.mat), c("4.5%", "50%", "95.5%"))
-		}
-	})
+# Only test whether ci.coords runs and returns without error.
+# Uses a very small number of iterations for speed
+# Doesn't test whether the results are correct.
+for (stratified in c(TRUE, FALSE)) {
+	for (test.roc in list(r.s100b, smooth(r.s100b))) {
+		test_that("ci.coords accepts one x and one ret", {
+			obtained <- ci.coords(test.roc, x=0.8, input = "sensitivity", ret="sp", 
+								  boot.n=3, conf.level = .91, boot.stratified = stratified)
+			expect_equal(attr(obtained, "ret"), "specificity")
+			expect_equal(names(obtained), attr(obtained, "ret"))
+			for (ci.mat in obtained) {
+				expect_equal(dim(ci.mat), c(1, 3))
+				expect_equal(colnames(ci.mat), c("4.5%", "50%", "95.5%"))
+			}
+		})
+		
+		test_that("ci.coords accepts one x and multiple ret", {
+			obtained <- ci.coords(test.roc, x=0.8, input = "sensitivity", ret=c("sp", "ppv", "tp", "1-sensitivity"), 
+								  boot.n=3, conf.level = .91, boot.stratified = stratified)
+			expect_equal(attr(obtained, "ret"), c("specificity", "ppv", "tp", "1-sensitivity"))
+			expect_equal(names(obtained), attr(obtained, "ret"))
+			for (ci.mat in obtained) {
+				expect_equal(dim(ci.mat), c(1, 3))
+				expect_equal(colnames(ci.mat), c("4.5%", "50%", "95.5%"))
+			}
+		})
+		
+		test_that("ci.coords accepts multiple x and one ret", {
+			obtained <- ci.coords(test.roc, x=c(0.8, 0.9), input = "sensitivity", ret="sp", 
+								  boot.n=3, conf.level = .91, boot.stratified = stratified)
+			expect_equal(attr(obtained, "ret"), "specificity")
+			expect_equal(names(obtained), attr(obtained, "ret"))
+			for (ci.mat in obtained) {
+				expect_equal(dim(ci.mat), c(2, 3))
+				expect_equal(colnames(ci.mat), c("4.5%", "50%", "95.5%"))
+			}
+		})
+		
+		test_that("ci.coords accepts multiple x and ret", {
+			obtained <- ci.coords(test.roc, x=c(0.9, 0.8), input = "sensitivity", ret=c("sp", "ppv", "tp", "1-se"), 
+								  boot.n=3, conf.level = .91, boot.stratified = stratified)
+			expect_equal(attr(obtained, "ret"), c("specificity", "ppv", "tp", "1-sensitivity"))
+			expect_equal(names(obtained), attr(obtained, "ret"))
+			for (ci.mat in obtained) {
+				expect_equal(dim(ci.mat), c(2, 3))
+				expect_equal(colnames(ci.mat), c("4.5%", "50%", "95.5%"))
+			}
+		})
+	}
 }
