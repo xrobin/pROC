79a8418b75d336ab3aac39fe6ffc42eb2a4d5a4f
Xavier Robin <xavalias-github@xavier.robin.name>
2020-01-19 15:55:46 +0100

    Test cov with obuchowski and bootstrap

--- tests/testthat/test-cov.R
+++ tests/testthat/test-cov.R
@@ -8,6 +8,13 @@ test_that("cov with delong works", {
 })
 
 
+test_that("cov with obuchowski works", {
+	expect_equal(cov(r.wfns, r.ndka, method = "obuchowski"), -3.917223e-06)
+	expect_equal(cov(r.ndka, r.s100b, method = "obuchowski"), 0.0007945308)
+	expect_equal(cov(r.s100b, r.wfns, method = "obuchowski"), 0.0008560803)
+})
+
+
 test_that("cov works with auc and mixed roc/auc", {
 	expect_equal(cov(auc(r.wfns), auc(r.ndka)), -0.000532967856762438)
 	expect_equal(cov(auc(r.ndka), r.s100b), -0.000756164938056579)
@@ -29,6 +36,13 @@ test_that("cov with delong, percent and mixed roc/auc works", {
 })
 
 
+test_that("cov with obuchowski, percent and mixed roc/auc works", {
+	expect_equal(cov(auc(r.wfns.percent), r.ndka.percent, method = "obuchowski"), -0.03917223)
+	expect_equal(cov(r.ndka.percent, auc(r.s100b.percent), method = "obuchowski"), 7.9453082)
+	expect_equal(cov(auc(r.s100b.percent), auc(r.wfns.percent), method = "obuchowski"), 8.560803)
+})
+
+
 test_that("cov with different auc specifications warns", {
 	expect_warning(cov(r.wfns, r.ndka.percent))
 	expect_warning(cov(r.wfns.percent, r.ndka))
@@ -40,9 +54,7 @@ test_that("cov with different auc specifications warns", {
 
 
 test_that("cov with delong, percent and direction = >", {
-	r1 <- roc(aSAH$outcome, -aSAH$ndka, percent=TRUE)
-	r2 <- roc(aSAH$outcome, -aSAH$s100b, percent=TRUE)
-	expect_equal(cov(r1, r2), -7.56164938056579)
+	expect_equal(cov(r.ndka.percent, r.s100b.percent), -7.56164938056579)
 })
 
 
@@ -53,4 +65,17 @@ test_that("cov with delong, percent, direction = > and mixed roc/auc", {
 	expect_equal(cov(auc(r1), auc(r2)), -7.56164938056579)
 	expect_equal(cov(auc(r1), r2), -7.56164938056579)
 	expect_equal(cov(r1, auc(r2)), -7.56164938056579)
+})
+
+
+test_that("cov with bootstrap works", {
+	skip_if_not(exists("run_slow_tests") && run_slow_tests, message = "Slow test skipped")
+	if (paste0(R.version$major, ".", R.version$minor) >= "3.6.0") {
+		RNGkind(sample.kind="Rounding")
+	}
+	set.seed(42)
+	expect_equal(cov(r.wfns, r.ndka, method = "bootstrap", boot.n = 100), -0.0004581385)
+	expect_equal(cov(r.ndka.percent, r.s100b.percent, method = "bootstrap", boot.n = 100), -6.312029126)
+	expect_equal(cov(r.s100b.partial1, r.wfns.partial1, method = "bootstrap", boot.n = 100), 2.899627e-05)
+	expect_equal(cov(r.wfns, r.ndka, method = "bootstrap", boot.n = 100, boot.stratified = FALSE), -0.000419791)
 })
