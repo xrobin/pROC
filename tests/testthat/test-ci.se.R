7297f2feb5746cd2feab56dfd0954b5a0656179f
Xavier Robin <xavalias-github@xavier.robin.name>
2020-02-12 22:18:27 +0100

    Test ci.se and ci.sp

---
+++ tests/testthat/test-ci.se.R
@@ -0,0 +1,40 @@
+library(pROC)
+data(aSAH)
+
+context("ci.se")
+
+# Only test whether ci.se runs and returns without error.
+# Uses a very small number of iterations for speed
+# Doesn't test whether the results are correct.
+
+# Silence progress bars
+options(pROCProgress = list(name = "none"))
+
+
+for (stratified in c(TRUE, FALSE)) {
+	test_that("ci.se with default specificities", {
+		n <- round(runif(1, 3, 9)) # keep boot.n small
+		obtained <- ci.se(r.ndka, boot.n = n, 
+						  boot.stratified = stratified, conf.level = .91)
+		expect_is(obtained, "ci.se")
+		expect_is(obtained, "ci")
+		expect_equal(dim(obtained), c(11, 3))
+		expect_equal(attr(obtained, "conf.level"), .91)
+		expect_equal(attr(obtained, "boot.n"), n)
+		expect_equal(colnames(obtained), c("4.5%", "50%", "95.5%"))
+		expect_equal(attr(obtained, "boot.stratified"), stratified)
+	})
+	
+	test_that("ci.se accepts one specificity", {
+		n <- round(runif(1, 3, 9)) # keep boot.n small
+		obtained <- ci.se(r.ndka, specificities = 0.9, boot.n = n, 
+						  boot.stratified = stratified, conf.level = .91)
+		expect_is(obtained, "ci.se")
+		expect_is(obtained, "ci")
+		expect_equal(dim(obtained), c(1, 3))
+		expect_equal(attr(obtained, "conf.level"), .91)
+		expect_equal(attr(obtained, "boot.n"), n)
+		expect_equal(colnames(obtained), c("4.5%", "50%", "95.5%"))
+		expect_equal(attr(obtained, "boot.stratified"), stratified)
+	})
+}
---
+++ tests/testthat/test-ci.sp.R
@@ -0,0 +1,40 @@
+library(pROC)
+data(aSAH)
+
+context("ci.sp")
+
+# Only test whether ci.sp runs and returns without error.
+# Uses a very small number of iterations for speed
+# Doesn't test whether the results are correct.
+
+# Silence progress bars
+options(pROCProgress = list(name = "none"))
+
+
+for (stratified in c(TRUE, FALSE)) {
+	test_that("ci.sp with default sensitivities", {
+		n <- round(runif(1, 3, 9)) # keep boot.n small
+		obtained <- ci.sp(r.ndka, boot.n = n, 
+						  boot.stratified = stratified, conf.level = .91)
+		expect_is(obtained, "ci.sp")
+		expect_is(obtained, "ci")
+		expect_equal(dim(obtained), c(11, 3))
+		expect_equal(attr(obtained, "conf.level"), .91)
+		expect_equal(attr(obtained, "boot.n"), n)
+		expect_equal(colnames(obtained), c("4.5%", "50%", "95.5%"))
+		expect_equal(attr(obtained, "boot.stratified"), stratified)
+	})
+	
+	test_that("ci.sp accepts one sensitivity", {
+		n <- round(runif(1, 3, 9)) # keep boot.n small
+		obtained <- ci.sp(r.ndka, sensitivities = 0.9, boot.n = n, 
+						  boot.stratified = stratified, conf.level = .91)
+		expect_is(obtained, "ci.sp")
+		expect_is(obtained, "ci")
+		expect_equal(dim(obtained), c(1, 3))
+		expect_equal(attr(obtained, "conf.level"), .91)
+		expect_equal(attr(obtained, "boot.n"), n)
+		expect_equal(colnames(obtained), c("4.5%", "50%", "95.5%"))
+		expect_equal(attr(obtained, "boot.stratified"), stratified)
+	})
+}
