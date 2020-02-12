451bd4b98515a88131646f09c553a4eb46d213b7
Xavier Robin <xavalias-github@xavier.robin.name>
2020-02-12 22:34:48 +0100

    Test ci.se/sp with smooth.roc

--- tests/testthat/test-ci.se.R
+++ tests/testthat/test-ci.se.R
@@ -12,29 +12,31 @@ options(pROCProgress = list(name = "none"))
 
 
 for (stratified in c(TRUE, FALSE)) {
-	test_that("ci.se with default specificities", {
-		n <- round(runif(1, 3, 9)) # keep boot.n small
-		obtained <- ci.se(r.ndka, boot.n = n, 
-						  boot.stratified = stratified, conf.level = .91)
-		expect_is(obtained, "ci.se")
-		expect_is(obtained, "ci")
-		expect_equal(dim(obtained), c(11, 3))
-		expect_equal(attr(obtained, "conf.level"), .91)
-		expect_equal(attr(obtained, "boot.n"), n)
-		expect_equal(colnames(obtained), c("4.5%", "50%", "95.5%"))
-		expect_equal(attr(obtained, "boot.stratified"), stratified)
-	})
-	
-	test_that("ci.se accepts one specificity", {
-		n <- round(runif(1, 3, 9)) # keep boot.n small
-		obtained <- ci.se(r.ndka, specificities = 0.9, boot.n = n, 
-						  boot.stratified = stratified, conf.level = .91)
-		expect_is(obtained, "ci.se")
-		expect_is(obtained, "ci")
-		expect_equal(dim(obtained), c(1, 3))
-		expect_equal(attr(obtained, "conf.level"), .91)
-		expect_equal(attr(obtained, "boot.n"), n)
-		expect_equal(colnames(obtained), c("4.5%", "50%", "95.5%"))
-		expect_equal(attr(obtained, "boot.stratified"), stratified)
-	})
+	for (test.roc in list(r.s100b, smooth(r.s100b))) {
+		test_that("ci.se with default specificities", {
+			n <- round(runif(1, 3, 9)) # keep boot.n small
+			obtained <- ci.se(test.roc, boot.n = n, 
+							  boot.stratified = stratified, conf.level = .91)
+			expect_is(obtained, "ci.se")
+			expect_is(obtained, "ci")
+			expect_equal(dim(obtained), c(11, 3))
+			expect_equal(attr(obtained, "conf.level"), .91)
+			expect_equal(attr(obtained, "boot.n"), n)
+			expect_equal(colnames(obtained), c("4.5%", "50%", "95.5%"))
+			expect_equal(attr(obtained, "boot.stratified"), stratified)
+		})
+		
+		test_that("ci.se accepts one specificity", {
+			n <- round(runif(1, 3, 9)) # keep boot.n small
+			obtained <- ci.se(test.roc, specificities = 0.9, boot.n = n, 
+							  boot.stratified = stratified, conf.level = .91)
+			expect_is(obtained, "ci.se")
+			expect_is(obtained, "ci")
+			expect_equal(dim(obtained), c(1, 3))
+			expect_equal(attr(obtained, "conf.level"), .91)
+			expect_equal(attr(obtained, "boot.n"), n)
+			expect_equal(colnames(obtained), c("4.5%", "50%", "95.5%"))
+			expect_equal(attr(obtained, "boot.stratified"), stratified)
+		})
+	}
 }
