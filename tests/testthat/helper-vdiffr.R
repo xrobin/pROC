ad4f3c5e0aee8d20cfcb1dc4ae749df6c05c1826
Xavier Robin <xavalias-github@xavier.robin.name>
2023-05-14 08:18:55 +0200

    Remove unused path argument

--- tests/testthat/helper-vdiffr.R
+++ tests/testthat/helper-vdiffr.R
@@ -1,6 +1,6 @@
 
 # Skip expect_doppelganger if vdiffr is not installed
-expect_doppelganger <- function(title, fig, path = NULL, ...) {
+expect_doppelganger <- function(title, fig, ...) {
 	testthat::skip_if_not_installed("vdiffr")
-	vdiffr::expect_doppelganger(title, fig, path = path, ...)
+	vdiffr::expect_doppelganger(title, fig, ...)
 }
