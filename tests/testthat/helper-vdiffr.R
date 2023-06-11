b47d8cda8913ac041075a2fc4ce0761c5f06717f
Xavier Robin <xavalias-github@xavier.robin.name>
2023-06-11 15:53:06 +0200

    Skip only ggroc test if ggplot2 is missing

--- tests/testthat/helper-vdiffr.R
+++ tests/testthat/helper-vdiffr.R
@@ -2,6 +2,11 @@
 # Skip expect_doppelganger if vdiffr is not installed
 expect_doppelganger <- function(title, fig, ...) {
 	testthat::skip_if_not_installed("vdiffr")
-	testthat::skip_if_not_installed("ggplot2")
 	vdiffr::expect_doppelganger(title, fig, ...)
+}
+
+# expect_doppelganger for ggroc
+expect_ggroc_doppelganger <- function(title, fig, ...) {
+	testthat::skip_if_not_installed("ggplot2")
+	expect_doppelganger(title, fig, ...)
 }
--- tests/testthat/test-ggroc.R
+++ tests/testthat/test-ggroc.R
@@ -6,28 +6,28 @@ test_that("Ggroc screenshot looks normal", {
 	test_ggplot_screenshot <- function() {
 		print(ggroc(r.s100b.percent, alpha = 0.5, colour = "red", linetype = 2, size = 2))
 	}
-	expect_doppelganger("ggroc.screenshot", test_ggplot_screenshot)
+	expect_ggroc_doppelganger("ggroc.screenshot", test_ggplot_screenshot)
 })
 
 test_that("Ggroc list screenshot looks normal", {
 	test_ggplot_list_screenshot <- function() {
 		print(ggroc(list(s100b=r.s100b, wfns=r.wfns, ndka=r.ndka)))
 	}
-	expect_doppelganger("ggroc.list.screenshot", test_ggplot_list_screenshot)
+	expect_ggroc_doppelganger("ggroc.list.screenshot", test_ggplot_list_screenshot)
 })
 
 test_that("Ggroc list can take multiple aes", {
 	test_ggplot_list_screenshot <- function() {
 		print(ggroc(list(s100b=r.s100b, wfns=r.wfns, ndka=r.ndka), aes=c("c", "linetype", "size")))
 	}
-	expect_doppelganger("ggroc.list.multi.aes", test_ggplot_list_screenshot)
+	expect_ggroc_doppelganger("ggroc.list.multi.aes", test_ggplot_list_screenshot)
 })
 
 test_that("Ggroc list extra aestetics screenshot looks normal", {
 	test_ggplot_list_extra_aes_screenshot <- function() {
 		print(ggroc(list(s100b=r.s100b, wfns=r.wfns, ndka=r.ndka), aes="linetype", color="red"))
 	}
-	expect_doppelganger("ggroc.list.extra.aes.screenshot", test_ggplot_list_extra_aes_screenshot)
+	expect_ggroc_doppelganger("ggroc.list.extra.aes.screenshot", test_ggplot_list_extra_aes_screenshot)
 })
 
 test_that("Ggroc list with group facet screenshot looks normal", {
@@ -36,7 +36,7 @@ test_that("Ggroc list with group facet screenshot looks normal", {
 		g <- ggroc(list(s100b=r.s100b, wfns=r.wfns, ndka=r.ndka), aes="group") + facet_grid(.~name)
 		print(g)
 	}
-	expect_doppelganger("ggroc.list.group.facet.screenshot", test_ggplot_list_group_facet_screenshot)
+	expect_ggroc_doppelganger("ggroc.list.group.facet.screenshot", test_ggplot_list_group_facet_screenshot)
 })
 
 test_that("Ggroc aesthetics can be modified with scale_colour_manual", {
@@ -44,7 +44,7 @@ test_that("Ggroc aesthetics can be modified with scale_colour_manual", {
 		print(ggroc(list(s100b=r.s100b, wfns=r.wfns, ndka=r.ndka), aes=c("c", "linetype")) +
 			  	scale_colour_manual(values = c("purple", "yellow", "purple")))
 	}
-	expect_doppelganger("ggroc.list.scale.colour.manual", test_ggplot_list_screenshot)
+	expect_ggroc_doppelganger("ggroc.list.scale.colour.manual", test_ggplot_list_screenshot)
 })
 
 
@@ -52,12 +52,12 @@ test_that("Ggroc screenshot looks normal with a single smooth.roc", {
 	test_ggplot_screenshot <- function() {
 		print(ggroc(smooth(r.s100b), , alpha = 0.5, colour = "red", linetype = 2, size = 2))
 	}
-	expect_doppelganger("ggroc.smooth.screenshot", test_ggplot_screenshot)
+	expect_ggroc_doppelganger("ggroc.smooth.screenshot", test_ggplot_screenshot)
 })
 
 test_that("Ggroc screenshot looks normal with a list of smooth.roc", {
 	test_ggplot_screenshot <- function() {
 		print(ggroc(list(s100b=smooth(r.s100b), wfns=smooth(r.wfns), ndka=smooth(r.ndka))))
 	}
-	expect_doppelganger("ggroc.smooth.list.screenshot", test_ggplot_screenshot)
+	expect_ggroc_doppelganger("ggroc.smooth.list.screenshot", test_ggplot_screenshot)
 })
