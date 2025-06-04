6c646c0a117f052e0bacbee91a2297bfd0f1414c
Michael Chirico <michaelchirico4@gmail.com>
2025-06-04 09:01:05 -0700

    rm plyr args

--- R/ci.thresholds.R
+++ R/ci.thresholds.R
@@ -83,10 +83,10 @@ ci.thresholds.roc <- function(roc,
     progress <- roc_utils_get_progress_bar(progress, title="Thresholds confidence interval", label="Bootstrap in progress...", ...)
 
   if (boot.stratified) {
-    perfs <- vapply(seq_len(boot.n), stratified.ci.thresholds, FUN.VALUE=double(2L), roc=roc, thresholds=thresholds.num, .progress=progress, .parallel=parallel)
+    perfs <- vapply(seq_len(boot.n), stratified.ci.thresholds, FUN.VALUE=double(2L), roc=roc, thresholds=thresholds.num)
   }
   else {
-    perfs <- vapply(seq_len(boot.n), nonstratified.ci.thresholds, FUN.VALUE=double(2L), roc=roc, thresholds=thresholds.num, .progress=progress, .parallel=parallel)
+    perfs <- vapply(seq_len(boot.n), nonstratified.ci.thresholds, FUN.VALUE=double(2L), roc=roc, thresholds=thresholds.num)
   }
 
   if (length(thresholds.num) > 1) {
