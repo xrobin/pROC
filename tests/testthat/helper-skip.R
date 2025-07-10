46e1582ef0bfe2133272fde37703e378f7e2dec7
Xavier Robin <xavalias-github@xavier.robin.name>
2025-07-10 16:52:48 +0200

    Fix running of slow tests

--- tests/testthat/helper-skip.R
+++ tests/testthat/helper-skip.R
@@ -1,4 +1,11 @@
 # Skip slow tests
-skip_slow <- function() {
-	skip_if_not(exists("run_slow_tests") && run_slow_tests, message = "Slow test skipped")
+skip_slow <- function(message = "Slow test skipped") {
+	if (exists("run_slow_tests", envir=.GlobalEnv)) {
+		if (! get("run_slow_tests", envir=.GlobalEnv)) {
+			skip(message)
+		}
+	}
+	else if (identical(Sys.getenv("RUN_SLOW_TESTS"), "true")) {
+		skip(message)
+	}
 }
